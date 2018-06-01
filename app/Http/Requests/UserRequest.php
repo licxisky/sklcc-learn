17439b582268aad1fb25844498ed50a91e86f24e
LiCxi <2992368059@qq.com>
2018-06-01 22:57:38 +0800

    Update

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -25,9 +25,26 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
+            'name' => 'required|between:4,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.unique' => ':attribute 已被占用，请重新填写',
+            'name.regex' => '用户名只支持英文、数字、横杆和下划线。',
+            'name.between' => '用户名必须介于 :min - :max 个字符之间。',
+            'name.required' => '用户名不能为空。',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'name' => "姓名"
         ];
     }
 }
